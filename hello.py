1a2f9025d951823f49e89b0d270e80e12e4aa8cd
Naohiro Tsuji <587586+Naohiro2g@users.noreply.github.com>
2022-03-29 20:58:47 +0900

    update

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ st.write("Welcome to my first app!")
 st.markdown("#### This is a markdown section")
 age = st.slider("How old are you?", 0, 100)
 st.write("So, you're %d years old." % age)
-fruit = st.selectbox("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
-fruit = st.multiselect("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
+# fruit1 = st.selectbox("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
+# fruit2 = st.multiselect("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
+# fruit1
+# fruit2
+
+st.balloons()
