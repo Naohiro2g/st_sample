d6bacf6bddeafbfeebf76b016dea69c5326e09e9
Naohiro Tsuji <587586+Naohiro2g@users.noreply.github.com>
2022-03-29 21:27:36 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Hello World!")
+# st.title("Hello World!")
 # st.write("Welcome to my first app!")
 # st.markdown("#### This is a markdown section")
-# age = st.slider("How old are you?", 0, 100)
-# st.write("So, you're %d years old." % age)
+age = st.slider("How old are you?", 0, 100)
+st.write("So, you're %d years old." % age)
 # fruit1 = st.selectbox("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
 # fruit2 = st.multiselect("What's your favorite fruit?", ["Apples", "Bananas", "Cherries"])
 # fruit1
